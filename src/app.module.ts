56e927f173a459f68bb4c6a32a7b627369e15bfa
Kenny P <kennyp2233@gmail.com>
2025-03-07 13:17:42 -0500

    feat: auth0 migration (to fix)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,26 @@
+// src/app.module.ts
+
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { UsuariosModule } from './users/users.module';
+import { Auth0Module } from './auth0/auth0.module';
+import { JwtStrategy } from './auth0/strategies/jwt.strategy';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    PrismaModule,
+    UsuariosModule,
+    Auth0Module,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, JwtStrategy],
 })
 export class AppModule {}
